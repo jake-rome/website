70b07df7f44303ad45b9b40672b7cd3334d0bc19
Jake Rome <jrome8602@gmail.com>
2025-07-20 19:47:43 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import OtherPage from './OtherPage';
+import { Navigate } from 'react-router-dom';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/other" element={<OtherPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
