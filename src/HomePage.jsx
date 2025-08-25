521ad4cc99a2d0d48dd17605241f29b74fa153e3
Jake Rome <jrome8602@gmail.com>
2025-08-25 14:52:11 -0400

    updates

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -136,7 +136,7 @@ function HomePage() {
   };
 
   const handleResumeClick = () => {
-    window.open(process.env.PUBLIC_URL + 'Jake Rome.pdf', '_blank')   
+    window.open(process.env.PUBLIC_URL + '/Jake Rome.pdf', '_blank')   
   }
 
   return (
